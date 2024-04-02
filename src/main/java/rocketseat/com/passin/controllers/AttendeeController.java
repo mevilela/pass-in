25c4379331cb457a253ce58a4cf7e6074f20de33
Duda Vilela <dudiqx@gmail.com>
2024-04-02 17:37:47 +0100

    Initial commit

---
+++ src/main/java/rocketseat/com/passin/controllers/AttendeeController.java
@@ -0,0 +1,17 @@
+package rocketseat.com.passin.controllers;
+
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attendees")
+public class AttendeeController {
+
+    @GetMapping
+    public ResponseEntity<String> getTest(){
+        return ResponseEntity.ok("sucesso!");
+    }
+}
---
+++ src/main/java/rocketseat/com/passin/dto/attendee/AttendeeDetailsDTO.java
@@ -0,0 +1,13 @@
+package rocketseat.com.passin.dto.attendee;
+
+import java.time.LocalDateTime;
+
+public record AttendeeDetailsDTO(
+        String id,
+        String name,
+        String email,
+        LocalDateTime createdAt,
+        LocalDateTime checkedInAt
+
+) {
+}
